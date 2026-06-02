85df117ca7f3b23f1b4b4bad877f367af7fdafa5
Natalie Spiva <natalie@acreetionos.org>
2026-06-02 10:35:37 +0000

    Bump SW cache to v7, add null check for header in sidebar.js
    Signed-off-by: Natalie Spiva <natalie@acreetionos.org>

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Service Worker for caching and offline support
-const CACHE_VERSION = 'v6';
+const CACHE_VERSION = 'v7';
 const CACHE_NAME = `acreetionos-${CACHE_VERSION}`;
 
 const STATIC_ASSETS = [
