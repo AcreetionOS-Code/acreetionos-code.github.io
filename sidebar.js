a51903b31dd98eb2365d33ffa6813ec60e348060
Natalie Spiva <natalie@acreetionos.org>
2026-06-02 11:14:09 +0000

    Add Authors section in sidebar with Natalie and Darren links
    Signed-off-by: Natalie Spiva <natalie@acreetionos.org>

--- sidebar.js
+++ sidebar.js
@@ -30,7 +30,8 @@ function init(){
     }
   }
   var externalSection='<div class="sidebar-section-title">External Projects</div><div class="sidebar-external"><a href="https://flatfree.pages.dev" target="_blank" rel="noopener" class="sidebar-external-btn"><i class="bi bi-box"></i> FlatFree</a></div>';
-  sidebar.innerHTML='<div class="sidebar-body"><ul>'+links+'</ul>'+externalSection+'</div><div class="sidebar-footer"><a href="https://discord.gg/VHqQkJASw7" target="_blank" rel="noopener">Discord</a><a href="contact.html">Contact</a></div>';
+  var authorsSection='<div class="sidebar-section-title">Authors</div><div class="sidebar-authors"><a href="https://natalie.acreetionos.org" target="_blank" rel="noopener" class="sidebar-author-link"><img src="natalie_avatar_new.png" alt="" class="sidebar-author-avatar"> Natalie Spiva</a><a href="https://darren.acreetionos.org" target="_blank" rel="noopener" class="sidebar-author-link"><img src="darren_avatar_new.png" alt="" class="sidebar-author-avatar"> Darren Clift</a></div>';
+  sidebar.innerHTML='<div class="sidebar-body"><ul>'+links+'</ul>'+externalSection+authorsSection+'</div><div class="sidebar-footer"><a href="https://discord.gg/VHqQkJASw7" target="_blank" rel="noopener">Discord</a><a href="contact.html">Contact</a></div>';
   document.body.appendChild(sidebar);
 }
 function toggle(){if(isOpen){close();}else{open();}}
