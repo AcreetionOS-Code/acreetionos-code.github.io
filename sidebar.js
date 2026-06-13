8dd8cdeee524cb0db8975ac8c68e359f32c63fee
Natalie Spiva <natalie@acreetionos.org>
2026-06-13 10:57:03 -0700

    Add flatfree.acreetionos.org link alongside flatfree.pages.dev in sidebar
    Both domains now serve the FlatFree Cloudflare Pages project, providing
    two URL options for SEO benefit.

--- sidebar.js
+++ sidebar.js
@@ -30,7 +30,7 @@ function init(){
       links+='<li><a href="'+h.replace(/&/g,'&amp;').replace(/"/g,'&quot;')+'">'+t.replace(/</g,'&lt;')+'</a></li>';
     }
   }
-  var externalSection='<div class="sidebar-section-title">External Projects</div><div class="sidebar-external"><a href="https://flatfree.pages.dev" target="_blank" rel="noopener" class="sidebar-external-btn"><i class="bi bi-box"></i> FlatFree</a></div>';
+  var externalSection='<div class="sidebar-section-title">External Projects</div><div class="sidebar-external"><a href="https://flatfree.pages.dev" target="_blank" rel="noopener" class="sidebar-external-btn"><i class="bi bi-box"></i> FlatFree</a> <a href="https://flatfree.acreetionos.org" target="_blank" rel="noopener" class="sidebar-external-btn" style="font-size:0.75em;opacity:0.7;"><i class="bi bi-globe2"></i> flatfree.acreetionos.org</a></div>';
   sidebar.innerHTML='<div class="sidebar-body"><ul>'+links+'</ul>'+externalSection+'</div><div class="sidebar-authors"><a href="https://darren.acreetionos.org" target="_blank" rel="noopener" class="sidebar-author-link"><img src="darren_avatar_new.png" alt="Darren Clift" class="sidebar-author-avatar"> Darren</a><a href="https://natalie.acreetionos.org" target="_blank" rel="noopener" class="sidebar-author-link"><img src="natalie_avatar_new.png" alt="Natalie Cole-Clift Spiva" class="sidebar-author-avatar"> Natalie</a></div><div class="sidebar-footer"><a href="https://discord.gg/VHqQkJASw7" target="_blank" rel="noopener">Discord</a><a href="contact.html">Contact</a></div>';
   document.body.appendChild(sidebar);
 }
