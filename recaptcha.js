b116ea02f4e5fbeedda2a29acea0d0e836748b5a
Natalie Spiva <natalie@acreetionos.org>
2026-09-02 15:26:46 -0700

    Fix Enterprise reCAPTCHA readiness

--- recaptcha.js
+++ recaptcha.js
@@ -1,1 +1,41 @@
-(function(){var i="6Lf-EoAtAAAAAI8dwkXHkdisu4eoz1KaZlFMK47w",o=!1,t=null;function c(){return o?Promise.resolve():t||(t=new Promise(function(r){var e=document.createElement("script");e.src="https://www.google.com/recaptcha/enterprise.js?render="+i,e.async=!0,e.defer=!0,e.onload=function(){o=!0,r()},e.onerror=function(){r()},document.head.appendChild(e)}),t)}window.getRecaptchaToken=function(r){return c().then(function(){return new Promise(function(e){if(!window.grecaptcha||!window.grecaptcha.enterprise){e(null);return}var n=!1,u=setTimeout(function(){n||(n=!0,e(null))},8e3);window.grecaptcha.enterprise.execute(i,{action:r||"submit"}).then(function(a){n||(n=!0,clearTimeout(u),e(a))},function(){n||(n=!0,clearTimeout(u),e(null))})})})}})();
+(function () {
+  'use strict';
+  var siteKey = '6Lf-EoAtAAAAAI8dwkXHkdisu4eoz1KaZlFMK47w';
+  var loadPromise = null;
+
+  function loadRecaptcha() {
+    if (window.grecaptcha && window.grecaptcha.enterprise && typeof window.grecaptcha.enterprise.execute === 'function') return Promise.resolve(window.grecaptcha.enterprise);
+    if (loadPromise) return loadPromise;
+    loadPromise = new Promise(function (resolve) {
+      var script = document.createElement('script');
+      script.src = 'https://www.google.com/recaptcha/enterprise.js?render=' + encodeURIComponent(siteKey);
+      script.async = true;
+      script.defer = true;
+      script.onload = function () {
+        var enterprise = window.grecaptcha && window.grecaptcha.enterprise;
+        resolve(enterprise && typeof enterprise.execute === 'function' ? enterprise : null);
+      };
+      script.onerror = function () { resolve(null); };
+      document.head.appendChild(script);
+    });
+    return loadPromise;
+  }
+
+  window.getRecaptchaToken = function (action) {
+    return loadRecaptcha().then(function (api) {
+      if (!api) return null;
+      return new Promise(function (resolve) {
+        var settled = false;
+        var timeout = setTimeout(function () { if (!settled) { settled = true; resolve(null); } }, 8000);
+        function execute() {
+          if (settled || typeof api.execute !== 'function') return;
+          Promise.resolve(api.execute(siteKey, { action: action || 'submit' })).then(function (token) {
+            if (!settled) { settled = true; clearTimeout(timeout); resolve(token); }
+          }).catch(function () { if (!settled) { settled = true; clearTimeout(timeout); resolve(null); } });
+        }
+        if (typeof api.ready === 'function') api.ready(execute);
+        else execute();
+      });
+    });
+  };
+})();
